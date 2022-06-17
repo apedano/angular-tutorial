d9f5328043c65a83a9403fe36ddd631402cb8a3a
Alessandro Pedano <pedano.alessandro@gmail.com>
2022-06-17 09:52:01 +0200

    Added Services compoments

---
+++ src/app/account-manager/new-account/new-account.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Account } from '../account.model';
+import { AccountService } from '../account.service';
+
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css']
+})
+export class NewAccountComponent {
+
+
+  constructor(private accountService: AccountService) {}
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountService.addAccount(new Account(accountName, accountStatus));
+  }
+}
